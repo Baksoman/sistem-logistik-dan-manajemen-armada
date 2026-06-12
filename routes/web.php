6d9df1af4a3a5861a1153c7ad17e61c1d95dda1a
clarenceevanw <clarenceevan0907@gmail.com>
2026-06-12 15:13:58 +0700

    feat: some routes

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
