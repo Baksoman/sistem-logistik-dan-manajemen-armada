53beaf4a3cfb1b4044e59b368aaf6320211203a6
clarenceevanw <clarenceevan0907@gmail.com>
2026-06-16 00:33:52 +0700

    fix: split layout logistik and driver

--- resources/views/logistik/routes/create.blade.php
+++ resources/views/logistik/routes/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.logistik')
 
 @section('title', 'Create Route')
 
--- resources/views/logistik/routes/show.blade.php
+++ resources/views/logistik/routes/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.logistik')
 
 @section('title', 'Route Details')
 
