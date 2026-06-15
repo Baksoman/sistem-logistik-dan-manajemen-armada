07173e703d876cbe9bfd3781bf8fec05adca44f2
clarenceevanw <clarenceevan0907@gmail.com>
2026-06-16 03:10:43 +0700

    fix: default tariff seeder

--- database/seeders/TariffSeeder.php
+++ database/seeders/TariffSeeder.php
@@ -15,24 +15,11 @@ class TariffSeeder extends Seeder
     {
         // 1. Direct Delivery Global Tariff (route_id = null)
         Tariff::create([
-            'price_per_km' => 2500, // Rp 2.500 per KM
-            'price_per_kg' => 1500, // Rp 1.500 per KG
+            'price_per_km' => 15000,
+            'price_per_kg' => 2500,
             'price_per_cbm' => 0,
-            'fixed_price' => 0,
+            'fixed_price' => 100000,
             'is_active' => true,
         ]);
-
-        // 2. Specific Tariffs for Master Routes
-        $routes = Route::all();
-        foreach ($routes as $route) {
-            Tariff::create([
-                'route_id' => $route->id,
-                'price_per_km' => 0, // Fixed cost logic used for Transit
-                'price_per_kg' => 500, // Rp 500 per KG weight penalty
-                'price_per_cbm' => 0,
-                'fixed_price' => 500000, // Rp 500.000 fixed price per transit
-                'is_active' => true,
-            ]);
-        }
     }
 }
